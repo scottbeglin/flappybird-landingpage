963b0ab12ed6d726f2fa5b4e3db401c7b7192c95
Scott Beglin <scott.beglin@gmail.com>
2016-01-16 22:03:00 -0800

    added command line features

--- gulpfile.js
+++ gulpfile.js
@@ -12,54 +12,54 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
 // JavaScript linting task
-gulp.task('jshint', function() {
-return gulp.src('site/js/*.js')
-.pipe(jshint())
-.pipe(jshint.reporter('default'));
+gulp.task('jshint', function () {
+    return gulp.src('site/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 // Compile Sass task
-gulp.task('sass', function() {
-return gulp.src('site/scss/*.scss')
-.pipe(sass())
-.pipe(gulp.dest('site/css'));
+gulp.task('sass', function () {
+    return gulp.src('site/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('site/css'));
 });
 
 // Minify index
-gulp.task('html', function() {
-return gulp.src('site/index.html')
-.pipe(minifyHTML())
-.pipe(gulp.dest('build/'));
+gulp.task('html', function () {
+    return gulp.src('site/index.html')
+        .pipe(minifyHTML())
+        .pipe(gulp.dest('build/'));
 });
 
 // JavaScript build task, removes whitespace and concatenates all files
-gulp.task('scripts', function() {
-return browserify('site/js/main.js').
-.bundle()
-.pipe(source('app.js'))
-.pipe(buffer())
-.pipe(uglify())
-.pipe(gulp.dest('build/js'));
+gulp.task('scripts', function () {
+    return browserify('site/js/main.js')
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
 });
 
 // Styles build task, concatenates all the files
-gulp.task('styles', function() {
-return gulp.src('site/css/*.css')
-.pipe(concat('styles.css'))
-.pipe(gulp.dest('build/css'));
+gulp.task('styles', function () {
+    return gulp.src('site/css/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('build/css'));
 });
 
 // Image optimization task
-gulp.task('images', function() {
-return gulp.src('site/img/*')
-.pipe(imagemin())
-.pipe(gulp.dest('build/img'));
+gulp.task('images', function () {
+    return gulp.src('site/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/img'));
 });
 
 // Watch task
-gulp.task('watch', function() {
-gulp.watch('site/js/*.js', ['jshint']);
-gulp.watch('site/scss/*.scss', ['sass']);
+gulp.task('watch', function () {
+    gulp.watch('site/js/*.js', ['jshint']);
+    gulp.watch('site/scss/*.scss', ['sass']);
 });
 
 // Default task
